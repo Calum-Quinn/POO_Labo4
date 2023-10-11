728e55a8dbdd51a787a9e37837acdd5f211c0a52
dylanramos <dylan.oliveiraramos@heig-vd.ch>
2023-10-11 15:28:16 +0200

    add bubble sort and print array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No arguments provided.");
+            return;
+        }
+
         int[] argsValues = new int[args.length];
 
         for (int i = 0; i < args.length; ++i) {
@@ -11,8 +16,8 @@ public class Main {
                 case '-':
                     isNegative = true;
                 case '+':
-                    j = 1;
-                    --power;
+                    j = 1; // If there is a sign, the first digit will be at index 1
+                    --power; // The first digit is at index 1, so the power is one less
                     break;
                 default:
                     j = 0;
@@ -20,7 +25,7 @@ public class Main {
 
             int number = 0;
             for (; j < arg.length(); ++j) {
-                number += (int)((arg.charAt(j) - '0') * Math.pow(10, power));
+                number += (int) ((arg.charAt(j) - '0') * Math.pow(10, power));
                 --power;
             }
 
@@ -29,7 +34,44 @@ public class Main {
             }
 
             argsValues[i] = number;
-            System.out.println(argsValues[i]);
         }
+
+        printArray(bubbleSort(argsValues));
+    }
+
+    private static int[] bubbleSort(int[] values) {
+        int temp;
+        boolean swapped;
+
+        for (int i = 0; i < values.length - 1; ++i) {
+            swapped = false;
+            // Loop on the digits that are not yet sorted
+            for (int j = 0; j < values.length - i - 1; ++j) {
+                if (values[j] > values[j + 1]) {
+                    // Swap the values
+                    temp = values[j];
+                    values[j] = values[j + 1];
+                    values[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // Breaks early if the array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+
+        return values;
+    }
+
+    private static void printArray(int[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; ++i) {
+            System.out.print(array[i]);
+            if (i != array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
     }
 }
