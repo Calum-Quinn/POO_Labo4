38e6dc7806b43f45b477e78f3da6a0e1befd1a59
dylanramos <dylan.oliveiraramos@heig-vd.ch>
2023-10-11 14:53:05 +0200

    get arguments

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] argsValues = new int[args.length];
 
+        for (int i = 0; i < args.length; ++i) {
+            String arg = args[i];
+            int j, power = arg.length() - 1;
+            boolean isNegative = false;
+
+            switch (arg.charAt(0)) {
+                case '-':
+                    isNegative = true;
+                case '+':
+                    j = 1;
+                    --power;
+                    break;
+                default:
+                    j = 0;
+            }
+
+            int number = 0;
+            for (; j < arg.length(); ++j) {
+                number += (int)((arg.charAt(j) - '0') * Math.pow(10, power));
+                --power;
+            }
+
+            if (isNegative) {
+                number *= -1;
+            }
+
+            argsValues[i] = number;
+            System.out.println(argsValues[i]);
+        }
+    }
+}
